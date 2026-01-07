3e6d9dd378c149a1070a372f84b6cec75a8ae992
AnarNajaf <necefli.anar420@gmail.com>
2026-01-08 01:07:32 +0400

    redesign

--- icons.js
+++ icons.js
@@ -1,10 +1,12 @@
 const sensorIcon = L.icon({
-  iconUrl: "../images/sensor.png",
+  iconUrl:
+    "https://anarnajaf.github.io/Map---Leaflet-JS---WEB-API/images/sensor.png",
   iconSize: [28, 36],
   iconAnchor: [16, 16],
 });
 const motorIcon = L.icon({
-  iconUrl: "../images/motor.png",
+  iconUrl:
+    "https://anarnajaf.github.io/Map---Leaflet-JS---WEB-API/images/motor.png",
   iconSize: [28, 36],
   iconAnchor: [16, 16],
 });
